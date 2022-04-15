f2020c3c51ea34a265c86ccc913c96906e2822d9
jeongwonyeong2 <wonyoung030930@gmail.com>
2022-04-15 11:02:11 +0900

    05 state , class 생명주기 학습

--- src/App.js
+++ src/App.js
@@ -1,73 +1,41 @@
 import React from 'react';
-import propTypes from 'prop-types';
 
-
-
-/*function Fruit(props){
-  console.log(props);
-  return (
-    <h1>{props.fav} 먹어</h1>
+class App extends React.Component {
+    constructor(props){
+      super(props)
+      console.log('constructor 실행완료');
+    }
     
-  )
-  }
-*/
+    state = {
+        count: 0,
+    };
 
+		add = () => {
+      this.setState({count:this.state.count+ 1})
+    };
 
-
-function Fruit({name,picture,rating}){
-  return (
-    <div>
-    <h2>{name}맛있어</h2>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={name} />
-  </div>
-  )
-  
+    minus = () => {
+      this.setState({count: this.state.count-1})
+    };
+    componentDidMount(){
+      console.log('componentDidMount 실행완료');
+    }
+    componentDidUpdate(){
+      console.log('componentDidUpdate 실행완료');
+    }
+    componentWillUnmount(){
+      console.log('비번');
+    }
+    render(){
+      console.log('render 실행완료');
+        return (
+            <div>
+                <h1> The number is: {this.state.count} </h1>
+                <button onClick={this.add}>더하기</button>
+                <button onClick={this.minus}>빼기</button>
+            </div>
+        );
+    }
 }
-const fruitLike=[
-  {
-    id:1,
-    name:'바나나',//키값이 있는애 객체
-    image:'http://qwerew.cafe24.com/images/banana.png',
-    rating:5,
-  },
-  { 
-    id:2,
-    name:'오렌지',//키값이 있는애 객체
-    image:'http://qwerew.cafe24.com/images/orange.png',
-    rating:4,
 
-  },
-  {
-    id:3,
-    name:'사과',//키값이 있는애 객체
-    image: 'http://qwerew.cafe24.com/images/apple.png',
-    rating:4.5,
-
-  },
-  {
-    id:4,
-    name:'멜론',//키값이 있는애 객체
-    image: 'http://qwerew.cafe24.com/images/melon.jpg',
-    rating:4.9,
-  },
-]
-/*
-function renderFruit(dish){
-  return<Fruit key={dish.id} name={dish.name} picture={dish.imae}/>
-}
-*/
-function App() {
-  return (
-  <div>
-    <Fruit key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
-  </div> 
-  )
-  
-}
-Fruit.propType={
-  name:propTypes.string.isRequired,
-  picture:propTypes.string.isRequired,
-  rating:propTypes.number.isRequired,
-}
 export default App;

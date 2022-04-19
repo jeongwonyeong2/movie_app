ae0e2190268c99e35b1b9fe76c3cabd5f9c59015
jeongwonyeong2 <wonyoung030930@gmail.com>
2022-04-19 11:37:45 +0900

    07리액트 업로드

--- src/Movie.js
+++ src/Movie.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { url } from 'inspector';
+import './Movie.css';
 
-function Movies(){
-    return <h1></h1>;
+function Movie({ id, title, year, summary, poster }) {
+    return (
+        <div class="movie">
+            <img src={poster} alt={title} title={title} />
+            <div class="movie__data">
+                <h3 class="movie__title"style={{backgroundColor: "red"}}>{title}</h3>
+                <h5 class="movie__year">{year}</h5>
+                <p class="movie__summary">{summary}</p>
+            </div>
+        </div>
+    );
 }
-//title,summery,images,medium_cover_image->poster
-Movies.propTypes={
-    id:PropTypes.number.isRequired,
-    year:PropTypes.number.isRequired,
-    title:PropTypes.string.isRequired,
-    summery:PropTypes.string.isRequired,
-    poster:PropTypes.string.isRequired,
-    
+
+Movie.propTypes = {
 };
+
 export default Movie;

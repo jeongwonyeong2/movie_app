8fc2f923434fef077344d035a73dc61a8cb2456a
jeongwonyeong2 <wonyoung030930@gmail.com>
2022-04-20 11:09:02 +0900

    07 깃허브에 리액트 앱 업로드하기

--- src/Movie.js
+++ src/Movie.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Movie.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({ id, title, year, summary, poster }) {
-    return (
-        <div class="movie">
-            <img src={poster} alt={title} title={title} />
-            <div class="movie__data">
-                <h3 class="movie__title"style={{backgroundColor: "red"}}>{title}</h3>
-                <h5 class="movie__year">{year}</h5>
-                <p class="movie__summary">{summary}</p>
-            </div>
-        </div>
-    );
+function Movie({ id, title, year, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre,index) => {
+            return <li key={index} className="movie_genre">{genre}</li>;
+          })}
+        </ul>
+        <p className="movie__summary">{summary.slice(0,180)}</p>
+      </div>
+    </div>
+  );
 }
 
-Movie.propTypes = {
-};
+Movie.propTypes = {};
 
 export default Movie;

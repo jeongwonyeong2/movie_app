c5f9ae292a183b64c10877b32746fa12154350b4
jeongwonyeong2 <wonyoung030930@gmail.com>
2022-04-22 16:51:48 +0900

    Updates

---
+++ precache-manifest.3c446c43490335797587cb814700017d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89186572982fb7a3b21060f5ec7671bf",
+    "url": "/jeongwonyeong2/movie_app.git/index.html"
+  },
+  {
+    "revision": "d01f7df2daac4823276a",
+    "url": "/jeongwonyeong2/movie_app.git/static/css/main.d021b36d.chunk.css"
+  },
+  {
+    "revision": "d761f6f4f496152e3a59",
+    "url": "/jeongwonyeong2/movie_app.git/static/js/2.6bb022e6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/jeongwonyeong2/movie_app.git/static/js/2.6bb022e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d01f7df2daac4823276a",
+    "url": "/jeongwonyeong2/movie_app.git/static/js/main.ca9c5623.chunk.js"
+  },
+  {
+    "revision": "4ff539e5b37b9279b72d",
+    "url": "/jeongwonyeong2/movie_app.git/static/js/runtime-main.e0994807.js"
+  }
+]);
